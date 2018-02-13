bb72507ba8091c4f76cffccf21625704068ead71
sevic69 <sebdeveloper6952@gmail.com>
2018-02-13 10:10:08 -0600

    Modificaciones en InputController.

---
+++ Assets/Scripts/CInputButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CInputButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler,
+    IPointerUpHandler
+{
+    public int buttonId;
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+            CInputController.instance.ButtonPressed(buttonId);
+    }
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        CInputController.instance.ButtonPressed(buttonId);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        CInputController.instance.ButtonReleased(buttonId);
+    }
+    
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        CInputController.instance.ButtonReleased(buttonId);
+    }
+}
