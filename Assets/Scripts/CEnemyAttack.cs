eda09570abb7b1ceb13165c5eab565c767e7632e
sevic69 <sebdeveloper6952@gmail.com>
2018-02-17 22:09:25 -0600

    Agregar sprites de posible enemigo y empezando comportamiento de enemigos.

---
+++ Assets/Scripts/CEnemyAttack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CEnemyAttack : MonoBehaviour 
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player entered enemy attack detection box.");
+        }
+    }
+}
---
+++ Assets/Scripts/CEnemyHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CEnemyHealth : MonoBehaviour
+{
+    public float maxHealth;
+
+    private float health;
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Enemy hit!");
+    }
+
+    public void MakeDamage(float amount)
+    {
+        if (amount > 0)
+        {
+            health -= amount;
+            if (health < 0)
+            {
+                health = 0;
+                // trigger death
+            }
+        }
+    }
+}
