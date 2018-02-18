f1c634236309ab65ec2d940e7b50836ebf27c98e
sebdeveloper6952 <sebdeveloper6952@gmail.com>
2018-02-18 09:04:35 -0600

    Scene transitions and camera script.

---
+++ Assets/Scripts/CFollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CFollowCamera : MonoBehaviour
+{
+	public GameObject target;
+	public float maxDistance = 1f;
+	public float camSpeed = 1f;
+
+	private void Update()
+	{
+		if (Vector2.Distance(target.transform.position, transform.position) > maxDistance)
+		{
+			Vector2 m = target.transform.position - transform.position;
+			transform.Translate(m * camSpeed * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/Scripts/CSceneUtilities.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine.SceneManagement;
+
+public class CSceneUtilities
+{
+    public static void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
