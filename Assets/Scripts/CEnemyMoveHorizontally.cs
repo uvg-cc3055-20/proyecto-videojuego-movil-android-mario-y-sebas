3e0086564a4302197e994e139fc51cd953970e45
sevic69 <sebdeveloper6952@gmail.com>
2018-02-21 12:43:36 -0600

    Agregar script para movimiento general de enemigos.

---
+++ Assets/Scripts/CEnemyMoveHorizontally.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CEnemyMoveHorizontally : MonoBehaviour 
+{
+    public float speed = 1f;
+    
+    private sbyte direction;
+    private SpriteRenderer sprite;
+
+    #region Unity Callbacks
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        direction = 1;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * direction * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        ReverseDirection();
+    }
+
+    #endregion
+    
+    #region Private Methods
+    private void ReverseDirection()
+    {
+        direction *= -1;
+        transform.localScale = new Vector3(direction, 1f, 1f);
+    }
+    #endregion
+}
