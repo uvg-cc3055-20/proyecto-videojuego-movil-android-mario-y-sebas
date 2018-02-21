68a4e6c690c160d4b861f24343bae8556020eb7b
sevic69 <sebdeveloper6952@gmail.com>
2018-02-21 12:44:02 -0600

    Generador de enemigos que caen del cielo.

---
+++ Assets/Scripts/CEnemyInstantiateFromSky.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CEnemyInstantiateFromSky : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public Transform spawnPoint;
+    [Tooltip("Maximum enemies this spawner can instantiate.")]
+    public int spawnAmount;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && (spawnAmount--) > 0)
+        {
+            Instantiate(enemies[Random.Range(0, enemies.Length)], spawnPoint.position, Quaternion.identity);   
+        }
+    }
+}
